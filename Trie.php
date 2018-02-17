257baed4bacc1379238c72f04c7e46031c25c35f
Dinesh Sisodiya <35620721+DineshSisodiya@users.noreply.github.com>
2018-02-17 16:22:19 +0530

    initial commit

--- Trie.php
+++ Trie.php
@@ -30,7 +30,6 @@ class Trie
 	{
 
 		$str=strtolower($str);//avoid case sensitivity
-		$ignore_chars=['.','&','!','@','-','_','`','~','#','$','*','(',')','+','-','/','\\','|','{','}','[',']','"','<','>','?'];
 		
 		$str_len=strlen($str);
 		if($this->root!=null)
@@ -38,9 +37,6 @@ class Trie
 			$temp=&$this->root;
 			for($i=0;$i<$str_len;$i++) 
 			{
-				if (in_array($str[$i], $ignore_chars)) {
-					continue;
-				}
 				if (array_key_exists($str[$i],$temp->child)) 
 				{
 					$temp=&$temp->child[$str[$i]];	
@@ -66,7 +62,7 @@ class Trie
 	/**
 	 *
 	 *@param $str : String
-	 *@return true if found else false
+	 *@return array([0]=>matched char. length [1]=>true if found else false)
 	 */
 	function search($str)
 	{
@@ -100,44 +96,11 @@ class Trie
 	}
 }
 
-// $ob=new Trie;
-// $ob->add('indian institute of management, ahmedabad');
-// $ob->add('iima');
-// $ob->add('faculty of management studies, university of delhi, delhi');
-// $ob->add('faculty of management studies');
-//$status=$ob->search('faculty of management studies');
+$ob=new Trie;
+$ob->add('indian institute of management, ahmedabad');
+$ob->add('iima');
+$ob->add('faculty of management studies, university of delhi, delhi');
+$ob->add('faculty of management studies');
+$status=$ob->search('faculty of management studies');
 
-
-// $post="faculty of management studies , delhi fms iimb sir i want to get iima iim admition in but i have a poor academic score in 2 i have 46% marks only and in graduation i have 51 % and in 10th i have 78 % so sir how can i now strengthen this weakness on mine while in pi i shouldn&#39t face much difficulty plz give some useful tips #gdpi18";
-// $kwArray=array();
-// $len=strlen($post);
-// $temp_str='';
-// $k=0;
-
-// for ($i=0; $i<$len; $i++) { 
-// 			if($post[$i]==' ') {
-// 				$status=$ob->search($temp_str);
-// 				if ($status[1]) {
-// 					$kwArray[$k++]=$temp_str;
-// 					$temp_str='';
-// 					print_r($kwArray);
-// 				} 
-// 				else if($status[0]==strlen($temp_str)) {
-// 					//echo '<br>'.$temp_str.'<br>';
-// 					$temp_str .= $post[$i];
-// 				}
-// 				else {
-// 					$temp_str='';
-// 				}
-// 			} 
-// 			else {
-// 				$temp_str .= $post[$i];
-// 			}
-// 		}
-
-
-
-//for checking execution time of the script
-// $time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
-// echo "Process Time: {$time}";
 ?>
